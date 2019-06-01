c510c0556ed32716e15c228c16f09553c93896ab
alpha1226 <rnduddn1226@gmail.com>
2019-06-02 00:35:46 +0900

    서버  연동

--- src/com/cafe_pol/kiosk/Client.java
+++ src/com/cafe_pol/kiosk/Client.java
@@ -16,33 +16,45 @@ public class Client {
 
     public static void main(String[] args) {
         Client cl = new Client();
-        for(int i=0;i<5;i++){
-            cl.ClientRun(i+"/A/b/C/D/E/F/G");
+        try{
+            cl.SocketStart();
+            cl.ClientRun("alpha Data");
+            cl.SocketClose();
+
+        } catch(Exception e){
+            e.printStackTrace();
         }
+
         //cl.ClientRun("0/alpha/beta/C/D/E/F/G");
         //cl.ClientRun("1/A/B/ce/de/ee/fe/ge");
         //cl.ClientRun("quit");
     }
 
-    public Client() {
-        try{socket = new Socket("localhost",4201);} catch (Exception e) {e.printStackTrace();}
+    public void SocketStart(){
+        try {
+            socket = new Socket("localhost", 4201);
+            System.out.println("Socket Open");
+        } catch (Exception e) {e.printStackTrace();}
     }
 
     public void ClientRun(String data) {
         try {
             //socket = new Socket("localhost", 4201);
+
             os = socket.getOutputStream();
             osw = new OutputStreamWriter(os);
-            bw = new BufferedWriter(osw);            //서버로 전송을 위한 OutputStream
-
+            bw = new BufferedWriter(osw);
+            //서버로 전송을 위한 OutputStream
+            System.out.println("os, osw, bs 생성완료");
             //is = socket.getInputStream();
             //isr = new InputStreamReader(is);
             //br = new BufferedReader(isr);        // 서버로부터 Data를 받음
 
-
             bw.write(data);
             bw.newLine();
             bw.flush();
+
+            System.out.println("to server : "+data);
 
             //String receiveData = "";
             //receiveData = br.readLine();        // 서버로부터 데이터 한줄 읽음
@@ -51,15 +63,17 @@ public class Client {
             e.printStackTrace();
         } finally {
             try {
-                bw.close();
-                osw.close();
-                os.close();
-                br.close();
-                isr.close();
-                is.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+    }
+    public void SocketClose(){
+        try {
+            bw.close();
+            osw.close();
+            os.close();
+            System.out.println("Socket Close");
+        } catch (Exception e) {e.printStackTrace();}
     }
 }
